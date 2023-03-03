f0f8375c2c1f8e506f7961da354d0050f8f583c7
Daniel Ramirez Chiquillo <hi@danielrachi.com>
2023-03-02 23:38:32 -0500

    tinytemplate experiments

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,31 @@ use std::{
     path::PathBuf,
 };
 
+use serde::Serialize;
+
+use std::error::Error;
+use tinytemplate::TinyTemplate;
+
+#[derive(Serialize)]
+struct Context {
+    title: String,
+}
+
+static TEMPLATE: &'static str = r#"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>{title}</h1>
+</body>
+</html>
+"#;
+
 /// Get the path of all the files in a given folder.
 fn get_paths(dir: PathBuf) -> io::Result<Vec<PathBuf>> {
     let mut paths: Vec<PathBuf> = Vec::new();
@@ -21,7 +46,7 @@ fn get_paths(dir: PathBuf) -> io::Result<Vec<PathBuf>> {
     Ok(paths)
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let paths = get_paths(PathBuf::from("content")).unwrap();
     let _: Vec<_> = paths
         .into_iter()
@@ -33,4 +58,14 @@ fn main() {
             )
         })
         .collect();
+
+    let mut tt = TinyTemplate::new();
+    tt.add_template("hello", TEMPLATE)?;
+    let context = Context {
+        title: "Daniel".to_string(),
+    };
+    let rendered = tt.render("hello", &context)?;
+    fs::write("out/title.html", rendered)?;
+
+    Ok(())
 }
